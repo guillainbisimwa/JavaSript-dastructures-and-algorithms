8a505f2bd69664e4115a113c3a1958c1e6ba1dc5
Your Name <you@example.com>
2023-01-03 22:45:58 +0200

    Init countingvalue fuction

---
+++ HackerRank/countingValue.js
@@ -0,0 +1,11 @@
+/**
+ * The f(x) is expected to return an INTEGER
+ * The f(x) accepts followings parameters:
+ * 1. INTEGER steps
+ * 2. SRING path
+ * 
+ */
+
+const countingValue = () => {
+    return 1
+}
