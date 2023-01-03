db55e0653c3028b4c2e5cbd48cce1656098f98e4
Your Name <you@example.com>
2023-01-03 20:06:40 +0200

    Init PileSocks file

---
+++ HackerRank/pileSocks.js
@@ -0,0 +1,19 @@
+// HackerRank
+// SockMerchant
+/**
+ * The function is expected to return an INTERGER
+ * The function accepts following parameters: 
+ * 
+ * 1. INTERGER n
+ * 2. INTERGER_ARRAY ar
+ * 
+ *  */ 
+
+ const SockMerchant = (n, ar) => {
+    
+    return n;
+};
+
+const s = SockMerchant(20, [1,2,1,2,1,3,2]);
+
+console.log(s);
